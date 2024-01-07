437dc048895e19915d1491c7e74a11ce2d0d2e93
ManuOmulo <omuloemmanuel@gmail.com>
2024-01-07 20:37:07 +0300

    Added reverse method

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
     System.out.println(Arrays.toString(arr));
     int minValue = findMin(arr);
     System.out.println("min value in array is: " + minValue);
+
+    System.out.println(Arrays.toString(reverse(arr)));
   }
 
   public static int[] readIntegers() {
@@ -33,5 +35,29 @@ public class Main {
     }
 
     return min;
+  }
+
+  private static int[] reverse(int[] array) {
+    int[] reversedArray = Arrays.copyOf(array, array.length);
+    int maxIndex = reversedArray.length - 1;
+    int halfLength = reversedArray.length / 2;
+
+    for (int i = 0; i < halfLength; i++) {
+      int temp = reversedArray[i];
+      reversedArray[i] = reversedArray[maxIndex - i];
+      reversedArray[maxIndex - i] = temp;
+    }
+
+//    for (int i = 0; i < reversedArray.length; i++) {
+//      for (int j = 1; j < reversedArray.length; j++) {
+//        if (j > i) {
+//          int temp = reversedArray[i];
+//          reversedArray[i] = reversedArray[j];
+//          reversedArray[j] = temp;
+//        }
+//      }
+//    }
+
+    return reversedArray;
   }
 }
