c6ddc7ad0dce641608232b4d45d4a2bb1c7e42ae
ManuOmulo <omuloemmanuel@gmail.com>
2024-01-07 15:33:13 +0300

    Created the readIntegers method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    System.out.println(Arrays.toString(readIntegers()));
+  }
 
+  public static int[] readIntegers() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter list of numbers separated by commas: ");
+    String input = sc.nextLine();
+    String[] formattedString = input.split(",");
+    int[] newIntArray = new int[formattedString.length];
+
+    for (int i = 0; i < newIntArray.length; i++) {
+      newIntArray[i] = Integer.parseInt(formattedString[i]);
+    }
+
+    return newIntArray;
   }
 }
